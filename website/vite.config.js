b57e7a0e6faafa264189758ae29d890ad18bb0e4
FahimAzh07 <fahimazh07@gmail.com>
2025-08-04 00:12:03 +0800

    Publish
    publish

--- website/vite.config.js
+++ website/vite.config.js
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import svgr from 'vite-plugin-svgr';
 
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/KlyftigWebsite/',
   plugins: [
     react(),
     tailwindcss(),
